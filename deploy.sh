b44847af73688e9f1ad8c844a110cb385f652299
Pierre Karashchuk <krchtchk@gmail.com>
2016-06-28 02:01:36 -0700

    one commit, not two

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 git checkout gh-pages
-git merge master -m 'merge'
+git merge master --no-commit
 webpack --optimize-occurence-order --optimize-dedupe --optimize-minimize
-git commit -am 'update build'
+git commit -am 'merge and update build'
 git push origin gh-pages
 git checkout master
