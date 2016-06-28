fd280a15e6616011f5f565628b87517e0fb66d95
Pierre Karashchuk <krchtchk@gmail.com>
2016-06-28 02:05:12 -0700

    fix script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 git branch -D gh-pages 
 git checkout -b gh-pages
 webpack --optimize-occurence-order --optimize-dedupe --optimize-minimize
-git add build
+git add -f build
 git commit -am 'update build'
 git push origin -f gh-pages
 git checkout master
